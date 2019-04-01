1d39aaec4f858238c5d870608e38f45e088a79bd
Dorokhov <id814@ic.ac.uk>
2019-04-01 18:58:53 +0100

    Fixed all major errors, added examples

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as p
 from scipy.integrate import solve_ivp
 import re #Regular expression module for reading reaction strings
 
@@ -9,29 +10,16 @@ class Chemistry:
         self.rate_constants = np.array([])
         self.orders = np.array([], ndmin = 2)
         self.species = dict()
-        self.m_in = 1 #inlet mass flow
+        self.c0 = dict()
         self.time_start = 0
-        self.time_stop = 10
-
-    def _m_in(self, t):
-        return self.m_in
-
-    def _m_out(self, t):
-        return self._m_in(t)
-
-    def _c_in(self, t):
-        pass
-
-    def _mass_balance(self, t, m):
-        dmdt = self._m_in(t) - self._m_out(t)
-        return dmdt
+        self.time_stop = 100
 
     def _concentration_balance(self, t, c):
-        #dcdt = self._m_in(t)*self._c_in(t) - self._m_out(t)*self._c_out(t) + self._c_gen(c)
         dcdt = self._c_gen(c)
         return dcdt
-    
+
     def rate(self, c):
+        #TODO: rewrite as array ** array
         r = np.zeros(len(self.rate_constants))
         i=0
         for reaction in self.orders:
@@ -48,13 +36,53 @@ class Chemistry:
         rates = self.rate(c)
         return np.dot(rates, self.stoichiometry)
 
+    def initial_concentrations(self, **kwargs):
+        #TODO error when kwards contains non-existing species
+        for species, conc in kwargs.items():
+            self.c0.update({species:conc})
+
+    def run(self):
+        self.ode_solver()
+        self.plot(all = True)
+
+    def plot(self, *args, all = False):
+        if all:
+            to_plot = self.species
+        else:
+            to_plot = args
+        fig = p.figure()
+        ax = fig.add_subplot(111)
+        ax.set_ylabel('concentration')
+        ax.set_xlabel('time')
+        for species in to_plot:
+            ax.plot(
+                self.solution['t'], 
+                self.solution[species],
+                label = species,
+                )
+        ax.legend()
+        p.show()
+    
     def ode_solver(self):
+
+        c0 = dict.fromkeys(self.species, 0)
+        c0.update({i:self.c0[i] for i in self.c0})
+        self.c0 = c0
+
         sol = solve_ivp(
             self._concentration_balance,
             [self.time_start,self.time_stop],
-            self.c0,
+            list(self.c0.values())
             #max_step = 0.1
             )
+        
+        self.solution = dict.fromkeys(self.species)
+        i = 0
+        for k in self.solution:
+            self.solution[k] = sol.y[i]
+            i+=1
+        self.solution['t'] = sol.t
+
         return sol.t, sol.y
 
     def reaction(self, string, k=1, k1=1, k2=1):
@@ -77,8 +105,13 @@ class Chemistry:
         else:
             print('reactions not found')
         
+        c0 = dict.fromkeys(self.species, 0)
+        c0.update({i:self.c0[i] for i in self.c0})
+        self.c0 = c0
+
     def _new_reaction(self, reagents, products, k):
-        
+        #TODO: rewrite from self.species to new dictionaries
+
         self.rate_constants = np.append(self.rate_constants, k)
 
         def coeff(s): 
@@ -115,6 +148,7 @@ class Chemistry:
 
         for i in self.species:
             self.species[i] = 0
+        
         self.species.update({species(i):abs(coeff(i)) for i in re.findall(r'[\.\w]+', reagents)})
         new_o = [self.species.get(i) for i in self.species]
         o = self.orders
@@ -125,3 +159,6 @@ class Chemistry:
             c[:o.shape[0], :o.shape[1]] = o
             c = np.vstack((c, new_o))
             self.orders = c
+        
+        for i in self.species:
+            self.species[i] = 0
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import models
-import matplotlib.pyplot as p
+import numpy as np
+
 #Brusselator test
 chem = models.Chemistry()
 
@@ -7,20 +8,23 @@ chem.reaction('A=>X')
 chem.reaction('2X+Y=>3X')
 chem.reaction('B+X=>Y+D')
 chem.reaction('X=>E')
-print(chem.stoichiometry)
-print(chem.species)
 
-# chem.stoichiometry[:,3] =np.zeros(4)
-# chem.stoichiometry[:,0] =np.zeros(4)
+chem.initial_concentrations(A=1, B=3, X=1, Y= 1)
+
+chem.stoichiometry[:,3] =np.zeros(4)
+chem.stoichiometry[:,0] =np.zeros(4)
 
 chem.time_stop = 100
-chem.c0 = [100,1,1,300,0,0]
-t, c = chem.ode_solver()
+chem.run()
+chem.plot('X', 'Y')
 
-j=0
-for i in c:
-    p.plot(t, i, label = list(chem.species.keys())[j])
-    j+=1
-p.legend()
-p.show()
+#Reversible reaction test
+chem2 = models.Chemistry()
 
+chem2.reaction('A=>B')
+chem2.reaction('A+X<=>C+D')
+chem2.reaction('B+Y<=>C+E')
+chem2.reaction('E+D<=>F')
+chem2.initial_concentrations(A=1, X=1, B=1, Y=1)
+chem2.time_stop = 50
+chem2.run()
