785bff6fee36200fad5853e77154d2926251a890
dorox <id814@ic.ac.uk>
2020-08-07 02:52:20 +0100

    version info, setup

--- chemreact/__init__.py
+++ chemreact/__init__.py
@@ -1,2 +1,4 @@
 from . import flowsheet, models, tools
 from .flowsheet import Flowsheet
+
+version = "0.0.3"
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="chemreact",
     version="0.0.3",
     author="Ilia Dorokhov",
@@ -12,9 +12,11 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dorox/react",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Operating System :: OS Independent",
-    ]
+    ],
+    python_requires = ">=3.6",
+    install_requires = ["numpy", "scipy", "matplotlib"]
 )
